979e76e3401518cf1e072c8bdffbfc4c3af305ab
Klim Misyuchenko <k.misyuchenko@gmail.com>
2013-09-24 13:32:47 +0300

    Omniture

--- src/Omniture/OmnitureData.js
+++ src/Omniture/OmnitureData.js
@@ -5,6 +5,6 @@
 		"prop1"    : "value of prop 1",
 		"eVar1"    : "value of evar 1",
                 "prop4"    : "value of prop 4",
-                "evap5"    : "evar 5 value"
+                "eVar5"    : "evar 5 value"
 	}
 ]
