87b01814d53dc30d0a80e0a053ed9413c2c1b49e
Klim Misyuchenko <k.misyuchenko@gmail.com>
2013-10-04 14:58:01 +0300

    Omniture test

--- src/Omniture/OmnitureConfig_Default.js
+++ src/Omniture/OmnitureConfig_Default.js
@@ -1,14 +1,14 @@
 [
     {
 		"omnitureEnabled" : "true",
-		"omnitureSuite"   : "viarnd",
+		"omnitureSuite"   : "test",
 		"omnitureChannel" : "Bento TestRig Channel",
 		"omnitureCharSet" : "ISO-8859-1",
 		"omnitureCurrencyCode" : "USD",
 		"omnitureSSL" : "",
 		"omnitureLifecycleSessionTimeout" : "300",
 		"omnitureTrackingServer" : "sc.mtv.com",
-		"omnitureVisitorID" : "",
+		"omnitureVisitorID" : "false",
 		"omnitureDebugLogging" : "true",
                 "demdexEnabled" : "true"
      }
