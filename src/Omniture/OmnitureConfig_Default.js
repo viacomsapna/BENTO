8aec283c1ebb5d5b09965e2cf048af5c7c7dc208
Klim Misyuchenko <k.misyuchenko@gmail.com>
2013-10-04 14:47:46 +0300

    Omniture testing

--- src/Omniture/OmnitureConfig_Default.js
+++ src/Omniture/OmnitureConfig_Default.js
@@ -5,7 +5,7 @@
 		"omnitureChannel" : "Bento TestRig Channel",
 		"omnitureCharSet" : "ISO-8859-1",
 		"omnitureCurrencyCode" : "USD",
-		"omnitureSSL" : "true",
+		"omnitureSSL" : "false",
 		"omnitureLifecycleSessionTimeout" : "300",
 		"omnitureTrackingServer" : "test.mtv.com",
 		"omnitureVisitorID" : "145215sfg4df5gdfgd5gd5f",
