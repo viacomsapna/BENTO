e586bd2edb169bf1d5c2cfb1cee2b5558d59b665
Klim Misyuchenko <k.misyuchenko@gmail.com>
2013-10-04 16:42:12 +0300

    Demdex

--- src/Omniture/OmnitureData.js
+++ src/Omniture/OmnitureData.js
@@ -1,10 +1,10 @@
 [
 	{
 		"pageName" : "test/bento/testrig",
-		"linkName" : "test/bento/link" /*,
+		"linkName" : "test/bento/link",
 		"prop1"    : "value of prop 1",
 		"eVar1"    : "value of evar 1",
                 "prop4"    : "value of prop 4",
-                "eVar5"    : "evar 5 value" */
+                "eVar5"    : "evar 5 value"
 	}
 ];
