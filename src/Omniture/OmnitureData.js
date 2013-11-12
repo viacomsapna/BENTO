d3b4e9d02d04b419d41f478b7606d46bcf0ec76b
Klim Misyuchenko <k.misyuchenko@gmail.com>
2013-11-12 16:28:46 +0300

    Omniture check

--- src/Omniture/OmnitureData.js
+++ src/Omniture/OmnitureData.js
@@ -4,7 +4,7 @@
 		"linkName" : "test/bento/link",
 		"prop1"    : "value of prop 1",
 		"eVar1"    : "value of evar 1",
-                "prop4"    : "value of prop 4",
-                "eVar5"    : "evar 5 value"
+        "prop4"    : "value of prop 4",
+        "eVar5"    : "evar 5 value"
 	}
 ];
