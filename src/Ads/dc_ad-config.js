8b5b47e7d7a37daa484405c4ed897dad4ac47627
Klim Misyuchenko <k.misyuchenko@gmail.com>
2013-11-14 16:25:00 +0300

    DoubleClick CTA complience

--- src/Ads/dc_ad-config.js
+++ src/Ads/dc_ad-config.js
@@ -4,6 +4,6 @@
         "doubleClickStaticAdAppId":"android_test_app",
         "doubleclickKeyValues":"test=testkeyvalue",
         "doubleclickUseExternalBrowser":"true",
-        "doubleClickStaticAdIsCoppaCompliant":true
+        "doubleClickStaticAdIsCoppaCompliant":false
     }
 ]
