79354c0f0e7ba29f3df2dbd60934b747e0cc1c92
Klim Misyuchenko <k.misyuchenko@gmail.com>
2013-11-26 10:58:25 +0300

    Retesting ENTGTANDRO-137
    http://jira.mtvi.com/browse/ENTGTANDRO-137 - P1: Reporting calls not
    using correct variables.

--- src/Omniture/OmnitureData.js
+++ src/Omniture/OmnitureData.js
@@ -5,6 +5,7 @@
 		"prop1"    : "value of prop 1",
 		"prop15"   : "value of prop 15",
 		"prop115"  : "value of prop 115",
+		"prop36"  : "value of prop 36 aka the show information",
 		"eVar1"    : "value of var 1",
 		"eVar16"   : "value of var 16",
 		"eVar116"  : "value of var 116"
