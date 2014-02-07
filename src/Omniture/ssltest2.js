05dd3dcfb0176dba2745a4fcd81a0e77f0e7dbdc
Klim Misyuchenko <k.misyuchenko@gmail.com>
2014-02-07 17:55:36 +0300

    ssltest2

---
+++ src/Omniture/ssltest2.js
@@ -0,0 +1,18 @@
+[
+    {
+        "omnitureEnabled" : "true",
+        "omnitureSuite"   : "viarnd",
+        "omnitureChannel" : "Bento TestRig Channel",
+        "omnitureCharSet" : "ISO-8859-1",
+        "omnitureCurrencyCode" : "USD",
+        "omnitureSSL" : "false",
+        "omnitureLifecycleSessionTimeout" : "3000",
+        "omnitureTrackingServer" : "sc.mtv.com",
+        "omnitureVisitorID" : "testcc8fb4594e6e98c4a9034a2f8f7d",
+        "omnitureDebugLogging" : "true",
+        "omnitureOfflineTrackingEnabled" : "true",
+        "demdexEnabled" : "true",
+        "TVEEnabled" : "true",
+        "omnitureContextData" : {"v.dummyContext1":"Klim Test Data","v.dummyContext2":"Klim_test_data"}
+    }
+]
