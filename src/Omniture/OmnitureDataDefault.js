a2da45aa13e75b229a48e5e125c0fd9a694c0fd9
Klim Misyuchenko <k.misyuchenko@gmail.com>
2014-02-07 17:26:20 +0300

    Omniture default data fix

---
+++ src/Omniture/OmnitureDataDefault.js
@@ -0,0 +1,16 @@
+[
+    {
+        "pageName" : "dev/bento/testrig",
+        "linkName" : "dev/bento/link",
+        "prop1"    : "value of prop 1",
+        "eVar1"    : "value of evar 1",
+        "prop11"    : "value of prop 11",
+        "eVar11"    : "value of evar 11",
+        "prop111"    : "value of prop 111",
+        "eVar111"    : "value of evar 111",
+        "prop74"    : "value of prop 74",
+        "eVar74"    : "value of evar 74",
+        "prop76"    : "value of prop 76",
+        "eVar76"    : "value of evar 76"
+    }
+]
