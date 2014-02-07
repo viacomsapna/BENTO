b686b9cbf7aabf121958a3387efb54ba95db6b89
Klim Misyuchenko <k.misyuchenko@gmail.com>
2014-02-07 16:37:52 +0300

    Adding samples for R 1.2.0

---
+++ src/Ads/doubleclick_config_300x50_no_dfp.js
@@ -0,0 +1,35 @@
+[
+{
+"doubleclickEnabled":true,
+"doubleClickStaticAdAppId":"android_test_app",
+"doubleclickKeyValues":"",
+"doubleclickUseExternalBrowser":false,
+"doubleClickStaticAdIsCoppaCompliant":true,
+"doubleclickAdSize":"300x50",
+"doubleclickIsDfp":false,
+"doubleclickDfpData":
+{
+    "metadata": {
+        "property": "mtv",
+        "hub": "shows",
+        "franchise": "Teen Mom 2",
+        "section": "full-episodes",
+        "title": "A Teen Mom Gets Pregnant.",
+        "is_main": true,
+        "season": 2,
+        "episode": "202"
+    },
+    "mappings": {
+        "ad-unit": {
+            "1": "@property"
+        },
+        "key-values": {
+            "sea": "@season",
+            "ep": "@episode",
+            "foo": "bar"
+        }
+    }
+
+}
+}
+]
---
+++ src/Omniture/app-config.js
@@ -0,0 +1,18 @@
+[
+	{
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "viarnd",
+		"omnitureChannel" : "Bento TestRig Channel",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USD",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "sc.mtv.com",
+		"omnitureVisitorID" : "",
+		"omnitureDebugLogging" : "true",
+		"omnitureOfflineTrackingEnabled" : "true",
+		"demdexEnabled" : "true",
+		"TVEEnabled" : "true",
+		"omnitureContextData" : {"v.dummyContext1":"1","v.dummyContext2":"2"}
+	}
+]
