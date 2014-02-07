ce97a7a152fbf67366b5d1f7db6bff3f82f963ec
Klim Misyuchenko <k.misyuchenko@gmail.com>
2014-02-07 17:23:40 +0300

    Test 1

---
+++ src/Omniture/OmnitureConfigDefault.js
@@ -0,0 +1,18 @@
+[
+    {
+        "omnitureEnabled" : "true",
+        "omnitureSuite"   : "viarnd",
+        "omnitureChannel" : "Bento TestRig Channel",
+        "omnitureCharSet" : "ISO-8859-1",
+        "omnitureCurrencyCode" : "USD",
+        "omnitureSSL" : "false",
+        "omnitureLifecycleSessionTimeout" : "300",
+        "omnitureTrackingServer" : "sc.mtv.com",
+        "omnitureVisitorID" : "",
+        "omnitureDebugLogging" : "true",
+        "omnitureOfflineTrackingEnabled" : "true",
+        "demdexEnabled" : "true",
+        "TVEEnabled" : "true",
+        "omnitureContextData" : {"v.dummyContext1":"Klim Test Data","v.dummyContext2":"Klim_test_data"}
+    }
+]
--- src/TVE/tve-config.js
+++ src/TVE/tve-config.js
@@ -10,7 +10,9 @@
 		"omnitureTrackingServer" : "sc.mtv.com",
 		"omnitureVisitorID" : "",
 		"omnitureDebugLogging" : "true",
+        "omnitureOfflineTrackingEnabled" : "true",
 		"demdexEnabled" : "true",
-		"TVEEnabled" : "true"
+		"TVEEnabled" : "true",
+        "omnitureContextData" : {"v.dummyContext1":"TVE Test","v.dummyContext2":"TVE_test_data"}
 	}
 ]
