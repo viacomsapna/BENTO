25759f6fc71c650cc487aa3243e74a7c41f5465d
Klim Misyuchenko <k.misyuchenko@gmail.com>
2014-02-17 18:11:46 +0300

    ADSM test

---
+++ src/iOS/Adms/adms_default.js
@@ -0,0 +1,30 @@
+[
+    {
+        "admsMeasureEnabled" : "true",
+        "admsReportSuiteIDs"   : "viarndtest",
+        "omnitureChannel" : "Bento TestRig Channel test",
+        "admsCharacterSet" : "ISO-8859-1",
+        "admsCurrencyCode" : "USD",
+        "admsSSLEnabled" : "false",
+        "admsLifecycleSessionTimeout" : "300",
+        "admsTrackingServer" : "test.mtv.com",
+        "admsVisitorID" : "",
+        "admsxDebugLoggingEnabled" : "true",
+        "admsOfflineTrackingEnabled" : "true",
+        "linkName" : "dev/bento/remote/link",
+        "prop1"    : "remote value of prop 1",
+        "pageName" : "dev/bento/remote/testrig",
+        "prop11"    : "remote value of prop 11",
+        "prop74"    : "remote value of prop 74",
+        "prop75"    : "remote value of prop 75",
+        "prop76"    : "remote value of prop 76",
+        "prop111"    : "remote value of prop 111",
+        "eVar1"    : "remote value of evar 1",
+        "eVar75"    : "remote value of evar 75",
+        "eVar76"    : "remote value of evar 76",
+        "eVar74"    : "remote value of evar 74",
+        "eVar111"    : "remote value of evar 111",
+        "eVar300"    : "remote value of evar 300",
+        "channel"  : "remote sandbox channel"
+    }
+]
