870214bbf172b10e3333e07f0d3d9a85219d754f
antalav <veantala@gmail.com>
2014-04-04 12:00:10 -0400

    Adding config file for MRAID Ad test with different test mode and size

---
+++ src/iOS/MRAIDAdConfig/medialets_mraid_728x90_expand.js
@@ -0,0 +1,41 @@
+[
+	{
+		"config" : {
+			"isCoppaCompliant" : true
+		},
+		"adData" : {
+			"x" : 0,
+			"y"   : 0,
+			"width" : 728,
+			"height" : 90,
+			"appName" : "btg_test_app",
+			"sections" : "",
+			"isDFP" : false,
+			"enableCloseButton" : true,
+			"closeButtonInterval" : 5,
+			"isRefreshable" : false,
+			"refreshInterval" : 10,
+			"keyValues" : {
+				"!category" : "v1.2.3",
+				"ver"   : "v1.2.3",
+				"testmodePrefix" : "medialets_mraid"
+			},
+			"keychainKeyValues" : {
+				"aamSegments" : ""
+			},
+			"dfpData" : {
+				"metadata" : {
+					"property" : "mtv_news",
+					"hub" : "breaking",
+					"is_main" : true
+				},
+				"mappings" : {
+					"ad-unit" : {
+						"1" : "@property",
+						"2" : "@hub"
+					}
+				}		        	
+			}
+		}
+	}
+]
