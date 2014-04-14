337fdf39f9cf824e551fc05ff5c281a9e7b19008
antalav <veantala@gmail.com>
2014-04-14 15:55:06 -0400

    Adding config to test slot value

---
+++ src/Android/Ads/300x250_ad_dfp_slot_2.js
@@ -0,0 +1,32 @@
+[
+{
+"doubleclickKeyValues":"",
+"doubleclickAdSize":"300x50",
+"doubleclickIsDfp":true,
+"doubleclickSlotNumber":2;
+"doubleclickDfpData":
+{
+    "metadata": {
+        "property": "mtv",
+        "hub": "shows",
+        "franchise": "Teen Mom 2",
+        "section": "full-episodes",
+        "title": "A Teen Mom Gets Pregnant.",
+        "is_main": true,
+        "season": 2,
+        "episode": "202"
+    },
+    "mappings": {
+        "ad-unit": {
+            "1": "@property"
+        },
+        "key-values": {
+            "sea": "@season",
+            "ep": "@episode",
+            "foo": "bar"
+        }
+    }
+
+}
+}
+]
