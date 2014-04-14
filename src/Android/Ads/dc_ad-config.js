62a8b89c678a0d539c14c01c2492b3e356ecc5fa
antalav <veantala@gmail.com>
2014-04-14 16:37:39 -0400

    set copa to false

--- src/Android/Ads/dc_ad-config.js
+++ src/Android/Ads/dc_ad-config.js
@@ -2,7 +2,6 @@
     {
         "doubleclickEnabled":"true",
         "doubleClickStaticAdAppId":"android_test_app",
-        "doubleclickKeyValues":"test=testkeyvalue",
         "doubleclickUseExternalBrowser":"true",
         "doubleClickStaticAdIsCoppaCompliant":false
     }
