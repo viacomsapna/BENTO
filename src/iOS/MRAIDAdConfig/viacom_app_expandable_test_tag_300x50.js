57d791dce4c111efb913cdd2a6585dd15b88c5de
antalav <veantala@gmail.com>
2014-05-12 18:29:44 -0400

    no message

---
+++ src/iOS/MRAIDAdConfig/viacom_app_expandable_test_tag_300x50.js
@@ -0,0 +1,41 @@
+[
+	{
+		"config" : {
+			"isCoppaCompliant" : true
+		},
+		"adData" : {
+			"x" : 0,
+			"y"   : 0,
+			"width" : 300,
+			"height" : 250,
+			"appName" : "btg_test_app",
+			"sections" : "",
+			"isDFP" : false,
+			"enableCloseButton" : true,
+			"closeButtonInterval" : 5,
+			"isRefreshable" : false,
+			"refreshInterval" : 10,
+			"keyValues" : {
+				"!category" : "v1.2.3",
+				"ver"   : "v1.2.3",
+				"testmode" : "viacom_app_expandable_test_tag_300x50"
+			},
+			"keychainKeyValues" : {
+				"aamSegments" : ""
+			},
+			"dfpData" : {
+				"metadata" : {
+					"property" : "mtv_news",
+					"hub" : "breaking",
+					"is_main" : true
+				},
+				"mappings" : {
+					"ad-unit" : {
+						"1" : "@property",
+						"2" : "@hub"
+					}
+				}		        	
+			}
+		}
+	}
+]
