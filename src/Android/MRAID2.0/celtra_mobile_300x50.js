649d7ff8d42fcadad470996a3b1b29924734ffc2
antalav <veantala@gmail.com>
2014-06-04 11:50:32 -0400

    no message

---
+++ src/Android/MRAID2.0/celtra_mobile_300x50.js
@@ -0,0 +1,31 @@
+[
+{
+"doubleclickKeyValues":"testmode=celtra_mobile_300x50",
+"doubleclickAdSize":"300x50",
+"doubleclickIsDfp":false,
+"doubleclickDfpData":
+{
+    "metadata": {
+        "property": "mtv",
+        "hub": "shows",
+        "franchise": "Teen Mom 2",
+        "section": "full-episodes",
+        "title": "A Teen Mom Gets Pregnant.",
+        "is_main": true,
+        "season": 2,
+        "episode": "202"
+    },
+    "mappings": {
+        "ad-unit": {
+            "1": "@property"
+        },
+        "key-values": {
+            "sea": "@season",
+            "ep": "@episode",
+            "foo": "bar"
+        }
+    }
+
+}
+}
+]
---
+++ src/Android/MRAID2.0/dap_celtra_mraid_728x90_expand.js
@@ -0,0 +1,31 @@
+[
+{
+"doubleclickKeyValues":"testmode=dap_celtra_mraid_728x90_expand",
+"doubleclickAdSize":"728x90",
+"doubleclickIsDfp":false,
+"doubleclickDfpData":
+{
+    "metadata": {
+        "property": "mtv",
+        "hub": "shows",
+        "franchise": "Teen Mom 2",
+        "section": "full-episodes",
+        "title": "A Teen Mom Gets Pregnant.",
+        "is_main": true,
+        "season": 2,
+        "episode": "202"
+    },
+    "mappings": {
+        "ad-unit": {
+            "1": "@property"
+        },
+        "key-values": {
+            "sea": "@season",
+            "ep": "@episode",
+            "foo": "bar"
+        }
+    }
+
+}
+}
+]
