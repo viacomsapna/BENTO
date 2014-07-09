5695415f5617e5de69776b691d8df07c9444a317
antalav <veantala@gmail.com>
2014-07-09 13:36:34 -0400

    Update OmnitureData.js

--- src/Android/Omniture/OmnitureData.js
+++ src/Android/Omniture/OmnitureData.js
@@ -2,13 +2,13 @@
 	{
 		"pageName" : "test/bento/testrig",
 		"linkName" : "test/bento/link",
-		"prop1"    : "value of prop 1",
-		"prop15"   : "value of prop 15",
-		"prop115"  : "value of prop 115",
-		"prop36"  : "value of prop 36 aka the show information",
-		"eVar1"    : "value of var 1",
-		"eVar16"   : "value of var 16",
-		"eVar116"  : "value of var 116",
+		"prop1"    : "value of prop  1 Data file",
+		"prop15"   : "value of prop 15 data file",
+		"prop115"  : "value of prop 115 data file",
+		"prop36"  : "value of prop 36 aka the show information data file",
+		"eVar1"    : "value of var 1 data file",
+		"eVar16"   : "value of var 16 data file",
+		"eVar116"  : "value of var 116 data file",
 		"omnitureContextData" : {"v.dummyContext11":"11","v.dummyContext22":"22"}
 	}
 ];
