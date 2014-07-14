d1ead84f782abca683fd6a7aa6621b34bf03003b
antalav <veantala@gmail.com>
2014-07-14 10:14:01 -0400

    Update adms_context_data.js

--- src/iOS/Adms/adms_context_data.js
+++ src/iOS/Adms/adms_context_data.js
@@ -3,7 +3,7 @@
 		"admsMeasureEnabled" : true,
 		"admsReportSuiteIDs"   : "viarnd",
 		"admsCharacterSet" : "ISO-8859-1",
-		"admsCurrencyCode" : "USD",
+		"admsCurrencyCode" : "USDD",
 		"admsSslEnabled" : false,
 		"admsLifecycleSessionTimeout" : 300,
 		"admsTrackingServer" : "sc.mtv.com",
