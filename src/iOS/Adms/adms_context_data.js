d9aa8d50de790034d17c515b9a64523c47d70fd0
antalav <veantala@gmail.com>
2014-07-16 10:12:36 -0400

    Update adms_context_data.js

--- src/iOS/Adms/adms_context_data.js
+++ src/iOS/Adms/adms_context_data.js
@@ -10,6 +10,9 @@
 		"admsVisitorID" : "",
 		"admsDebugLoggingEnabled" : true,
 		"admsOfflineTrackingEnabled" : true,
+		"prop11"    : "remote value of prop 11 onconfig",
+        	"prop12"    : "remote value of prop 12 onconfig",
+        	"eVar1"    : "remote value of evar 1 onconfig",
 		"admsContextData"   : {
 			"contextDataKey1onconfig"    : "testcontextvalue1onconfig",
 			"contextDataKey2onconfig"    : "testcontextvalue2onconfig"
