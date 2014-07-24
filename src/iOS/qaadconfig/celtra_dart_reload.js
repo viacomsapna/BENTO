609a43712a4aef1a6dd7562e60eec1538885b308
antalav <veantala@gmail.com>
2014-07-24 15:05:42 -0400

    no message

--- src/iOS/qaadconfig/celtra_dart_reload.js
+++ src/iOS/qaadconfig/celtra_dart_reload.js
@@ -10,7 +10,7 @@
 			"height" : 50,
 			"appName" : "btg_test_app",
 			"sections" : "",
-			"isDFP" : true,
+			"isDFP" : false,
 			"enableCloseButton" : true,
 			"closeButtonInterval" : 5,
 			"isRefreshable" : true,
