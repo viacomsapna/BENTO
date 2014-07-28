2f9901f77d9ef3703ec3f74b6a61368198f47ec3
antalav <veantala@gmail.com>
2014-07-28 15:38:35 -0400

    no message

---
+++ src/iOS/Adms/iOSAppConfig.js
@@ -0,0 +1,15 @@
+[
+	{
+		"admsMeasureEnabled" : true,
+		"admsReportSuiteIDs"   : "viarnd",
+		"admsCharacterSet" : "ISO-8859-1",
+		"admsCurrencyCode" : "USD",
+		"admsSslEnabled" : false,
+		"admsLifecycleSessionTimeout" : 300,
+		"admsTrackingServer" : "sc.mtv.com",
+		"admsVisitorID" : "",
+		"admsDebugLoggingEnabled" : true,
+		"admsOfflineTrackingEnabled" : true,
+		"audienceManagerEnabled" : true
+	}
+]
