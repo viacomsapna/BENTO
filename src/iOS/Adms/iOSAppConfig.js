1cd56730b63eda4d136785bd13c9210245208aba
antalav <veantala@gmail.com>
2014-07-29 07:31:56 -0400

    no message

--- src/iOS/Adms/iOSAppConfig.js
+++ src/iOS/Adms/iOSAppConfig.js
@@ -13,3 +13,5 @@
 		"audienceManagerEnabled" : true
 	}
 ]
+
+http://pubads.g.doubleclick.net/gampad/ads?gdfp_req=1&correlator=2394963211976704&output=json_html&callback=window.parent.googletag.impl.pubads.setAdContentsBySlotForAsync&impl=fifs&json_a=1&sfv=1-0-0&iu_parts=8675%2Cmtv_news_mob%2Cbreaking%2Chome&enc_prev_ius=0%2F1%2F2%2F3&prev_iu_szs=300x250&prev_scp=sdk%3D1.4.0%26ver%3D1.0.13-SNAPSHOT%26testmodePrefix%3Dceltra_mraid%26excl_cat%3D1.0.13-SNAPSHOT%26aam%3D1128158%2C1128161%2C1128170%26testmode%3Dceltra_mraid_300x250%26pos%3Datf%26slot%3D1&cookie_enabled=1&lmt=1406610456&dt=1406596056234&cc=100&frm=20&biw=300&bih=250&oid=3&adks=3972696886&gut=v2&ifi=1&u_tz=-240&u_his=1&u_h=1024&u_w=768&u_ah=1024&u_aw=748&u_cd=32&u_nplug=1&u_nmime=34&u_sd=2&flash=0&url=file%3A%2F%2F%2Fvar%2Fmobile%2FApplications%2F41E4CAB5-4E72-4185-8FF3-320DAEE27512%2FBentoTestRig.app%2FViacomMRAIDResources.bundle%2F&vrg=45&vrp=45&ga_vid=285133570.1406596056&ga_sid=1406596056&ga_hid=1141926822
