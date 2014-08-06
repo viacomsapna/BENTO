e5aad78df99c8e19eed165bd3f5cad314a2540a0
antalav <veantala@gmail.com>
2014-08-06 10:08:33 -0400

    no message

---
+++ src/iOS/Adms/trackpage_nocontext.js
@@ -0,0 +1,20 @@
+[
+{
+"pageName" : "dev/bento/remote/testrig",
+"channel" : "remote sandbox channel",
+"prop1" : "remote value of prop 1",
+        "prop11" : "remote value of prop 11 track page",
+        "prop74" : "remote value of prop 74 track page",
+        "prop75" : "remote value of prop 75 track page",
+        "prop76" : "remote value of prop 76 track page",
+        "prop111" : "remote value of prop 111",
+        "eVar1" : "remote value of evar 1 track page",
+        "eVar75" : "remote value of evar 75 track page",
+        "eVar76" : "remote value of evar 76 track page",
+        "eVar74" : "remote value of evar 74 track page",
+        "eVar111" : "remote value of evar 111 track page",
+        "eVar300" : "remote value of evar 300 track page",
+        "channel" : "remote sandbox channel track page"
+        }
+}
+]
