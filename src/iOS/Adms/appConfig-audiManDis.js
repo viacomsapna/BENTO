be1b04c2e5a8ebbb37c252a8c2f1e7e03b35cc5f
antalav <veantala@gmail.com>
2014-08-13 14:50:03 -0400

    no message

---
+++ src/iOS/Adms/appConfig-audiManDis.js
@@ -0,0 +1,20 @@
+[
+	{
+		"admsMeasureEnabled" : true,
+		"admsReportSuiteIDs"   : "viarnd",
+		"admsCharacterSet" : "ISO-8859-1",
+		"admsCurrencyCode" : "USD",
+		"admsSslEnabled" : true,
+		"admsLifecycleSessionTimeout" : 300,
+		"admsTrackingServer" : "sc.mtv.com",
+		"admsVisitorID" : "testVID",
+		"admsDebugLoggingEnabled" : true,
+		"admsOfflineTrackingEnabled" : true,
+		"audienceManagerEnabled" : false,
+		"admsContextData"   : {
+			"contextDataKey1onconfig"    : "testcontextvalue1onconfig",
+			"contextDataKey2onconfig"    : "testcontextvalue2onconfig",
+			"contextDataKey1trackpage"    : "testcontextvalue1onconfig"
+		}
+	}
+]
