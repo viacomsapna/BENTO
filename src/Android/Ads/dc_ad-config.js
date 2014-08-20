b3781d30dc06250454e611d65ac2750843a841d5
antalav <veantala@gmail.com>
2014-08-20 11:32:50 -0400

    Update dc_ad-config.js

--- src/Android/Ads/dc_ad-config.js
+++ src/Android/Ads/dc_ad-config.js
@@ -1,6 +1,9 @@
 [
 	{
-		"doubleclickEnabled":"true",
+		"omnitureEnabled": true,
+		"omnitureSuite:" : "viarnd",
+		"omnitureTrackingServer" : "sc.mtv.com",
+		"doubleclickEnabled":true,
 		"doubleClickStaticAdAppId":"android_test_app",
 		"doubleclickUseExternalBrowser":false,
 		"doubleClickStaticAdIsCoppaCompliant":false,
