4612c8811f8ed288101e1bb54315e1e422e06b25
antalav <veantala@gmail.com>
2014-09-12 11:38:18 -0400

    no message

---
+++ src/iOS/Ads/DFPAd.js
@@ -0,0 +1,47 @@
+[
+    {
+        "config": {
+            "isCoppaCompliant": false,
+            "audienceManagerEnabled": true
+        },
+        "adData": {
+            "x": 0,
+            "y": 0,
+            "width": 300,
+            "height": 250,
+            "appName": "btg_test_app",
+            "sections": "",
+            "isDFP": true,
+            "enableCloseButton": true,
+            "closeButtonInterval": 5,
+            "isRefreshable": false,
+            "refreshInterval": 10,
+            "keyValues": {
+                "testmodePrefix": ""
+            },
+            "keychainKeyValues": {},
+            "dfpData": {
+                "metadata": {
+                    "metadata": {
+                        "property": "nick",
+                        "is_main": true
+                    },
+                    "mappings": {
+                        "ad-unit": {
+                            "1": "@property",
+                            "2": "shows",
+                            "3": "victorious",
+                            "4": "home"
+                        }
+                    }
+                },
+                "alternativeSizes": [
+                    {
+                        "adWidth": 300,
+                        "adHeight": 250
+                    }
+                ]
+            }
+        }
+    }
+]
