86880ec21651715b8d6559003ebe2d0700a3b8f9
antalav <veantala@gmail.com>
2014-09-24 16:24:56 -0400

    no message

---
+++ src/BTG QA/Android/AdConfig/ad_appconfig_coppaTrue.js
@@ -0,0 +1,14 @@
+[
+	{
+		"omnitureEnabled": true,
+		"omnitureSuite:" : "viarnd",
+		"omnitureTrackingServer" : "sc.mtv.com",
+		"doubleclickEnabled":true,
+		"doubleClickStaticAdAppId":"android_test_app",
+		"doubleclickUseExternalBrowser":false,
+		"doubleClickStaticAdIsCoppaCompliant":true,
+		"doubleClickAdIndicatorVisible":true,
+		"doubleClickUseBumper":true,
+		"audienceManagerEnabled":true
+	}
+]
