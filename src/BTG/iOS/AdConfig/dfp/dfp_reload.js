52cada1176368983544540d4923b2197f11ca119
antalav <veantala@gmail.com>
2014-09-29 13:57:34 -0400

    Update dfp_reload.js

--- src/BTG/iOS/AdConfig/dfp/dfp_reload.js
+++ src/BTG/iOS/AdConfig/dfp/dfp_reload.js
@@ -16,6 +16,8 @@
 			"closeButtonInterval" : 5,
 			"isRefreshable" : true,
 			"refreshInterval" : 10,
+			"enableDefaultBumper" : true,
+			"enableCoppaAdBanner" : true,
 			"keyValues" : {
 				"!category" : "v1.2.3",
 				"ver"   : "v1.2.3",
