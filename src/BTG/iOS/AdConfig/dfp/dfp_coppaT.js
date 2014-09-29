1679635f73f644172c7919bf9d63925772000b06
antalav <veantala@gmail.com>
2014-09-29 13:43:41 -0400

    Update dfp_coppaT.js

--- src/BTG/iOS/AdConfig/dfp/dfp_coppaT.js
+++ src/BTG/iOS/AdConfig/dfp/dfp_coppaT.js
@@ -11,7 +11,7 @@
 			"height" : 250,
 			"appName" : "btg_test_app",
 			"sections" : "",
-			"isDFP" : true,
+			"isDFP" : false,
 			"enableCloseButton" : true,
 			"closeButtonInterval" : 5,
 			"isRefreshable" : false,
