775e5ec01c584c2a24a93688c334c6e52f78214a
antalav <veantala@gmail.com>
2014-09-29 14:20:45 -0400

    no message

--- src/BTG/iOS/AdConfig/dfp/dfp_differentAdsize.js
+++ src/BTG/iOS/AdConfig/dfp/dfp_differentAdsize.js
@@ -1,38 +1,61 @@
 [
 	{
 		"config" : {
-			"isCoppaCompliant" : false
+			"isCoppaCompliant" : true,
+                         "audienceManagerEnabled": true
+
 		},
 		"adData" : {
 			"x" : 0,
 			"y"   : 0,
-			"width" : 300, 320
-			"height" : 250, 50
+			"width" : 300,
+			"height" : 250,
 			"appName" : "btg_test_app",
 			"sections" : "",
 			"isDFP" : true,
 			"enableCloseButton" : true,
 			"closeButtonInterval" : 5,
-			"isRefreshable" : false,
+			"isRefreshable" : true,
 			"refreshInterval" : 10,
+			"alternativeSizes" : [
+					{
+					"adWidth" : 300,
+					"adHeight" : 50,
+					},
+					{
+					"adWidth" : 200,
+					"adHeight" : 100,
+					}
+			],
 			"keyValues" : {
 				"!category" : "v1.2.3",
 				"ver"   : "v1.2.3",
-				"testmodePrefix" : "celtra_mraid"
+				"testmodePrefix" : "medialets_mraid"
 			},
 			"keychainKeyValues" : {
 				"aamSegments" : ""
 			},
 			"dfpData" : {
 				"metadata" : {
-					"property" : "nick",
-					"hub" : "breaking",
-					"is_main" : true
+					  "property": "mtv",
+					  "hub": "shows",
+					  "franchise": "Teen Mom 2",
+        				  "section": "full-episodes",
+					  "title": "a-teen-mom-gets-pregnant.",
+        				  "is_main": true,
+					  "season": 2,
+        				  "episode": "202"
 				},
 				"mappings" : {
 					"ad-unit" : {
-						"1" : "@property"
-					}
+						"1" : "@property",
+						"2" : "@hub"
+					},
+					"key-values": {
+            					"sea": "@season",
+            					"ep": "@episode",
+        					 "foo": "bar"
+						 }
 				}		        	
 			}
 		}
