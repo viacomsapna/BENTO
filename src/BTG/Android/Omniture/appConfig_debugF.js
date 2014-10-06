7fdf30435bd419a510ae6ec5a9a85d6e0d527f5a
antalav <veantala@gmail.com>
2014-10-06 16:10:28 -0400

    Added new app config for debug False

---
+++ src/BTG/Android/Omniture/appConfig_debugF.js
@@ -0,0 +1,24 @@
+
+[
+	{
+		"audienceManagerEnabled" : "true",
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "viarnd",
+		"omnitureChannel" : "Test Rig Channel",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USD",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "sc.mtv.com",
+		"omnitureVisitorID" : "",
+		"omnitureDebugLogging" : "false",
+		"omnitureOfflineTrackingEnabled" : "true",
+		"omnitureOfflineHitLimit" : 1000,
+		"demdexEnabled" : "true",
+		"TVEEnabled" : "true",
+		"omnitureContextData" : {"v.dummyContext1":"Klim Test Data","v.dummyContext2":"Klim_test_data", "dummyContext12":"app12","nick.game": "app1"},
+		"omnitureTimetrackingEnabled" : "true",
+		"appIsCoppaCompliant" : "false"
+	}
+]
+
