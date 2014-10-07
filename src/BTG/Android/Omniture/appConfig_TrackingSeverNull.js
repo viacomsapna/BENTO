9daab89270133185d0c9fb219b23c02584940a7e
antalav <veantala@gmail.com>
2014-10-07 16:16:29 -0400

    adding config for testing tracking sever null

---
+++ src/BTG/Android/Omniture/appConfig_TrackingSeverNull.js
@@ -0,0 +1,22 @@
+[
+	{
+		"audienceManagerEnabled" : "true",
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "viarnd",
+		"omnitureChannel" : "TestChannel",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USD",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "",
+		"omnitureVisitorID" : "2",
+		"omnitureDebugLogging" : "true",
+		"omnitureOfflineTrackingEnabled" : "true",
+		"omnitureOfflineHitLimit" : 1000,
+		"demdexEnabled" : "true",
+		"TVEEnabled" : "true",
+		"omnitureTimetrackingEnabled" : "true",
+		"appIsCoppaCompliant" : "false"
+    	}
+]
+
