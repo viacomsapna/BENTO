6e6f283fd11dd1b1f3a18cddc6490f77b51dfed7
antalav <veantala@gmail.com>
2014-10-07 16:20:00 -0400

    adding config with suite null

---
+++ src/BTG/Android/Omniture/appConfig_OmniSuiteNull.js
@@ -0,0 +1,23 @@
+[
+	{
+		"audienceManagerEnabled" : "true",
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "",
+		"omnitureChannel" : "TestChannel",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USD",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "sc.mtv.com",
+		"omnitureVisitorID" : "2",
+		"omnitureDebugLogging" : "true",
+		"omnitureOfflineTrackingEnabled" : "true",
+		"omnitureOfflineHitLimit" : 1000,
+		"demdexEnabled" : "true",
+		"TVEEnabled" : "true",
+		"omnitureTimetrackingEnabled" : "true",
+		"appIsCoppaCompliant" : "false"
+    	}
+]
+
+
