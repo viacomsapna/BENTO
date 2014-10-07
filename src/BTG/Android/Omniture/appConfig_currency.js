864d92b4af4000bb6d1da608aada9ff4eb582edf
antalav <veantala@gmail.com>
2014-10-07 13:13:49 -0400

    adding new configs for omniture testing

---
+++ src/BTG/Android/Omniture/appConfig_currency.js
@@ -0,0 +1,21 @@
+[
+	{
+		"audienceManagerEnabled" : "true",
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "viarnd",
+		"omnitureChannel" : "TestChannel",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "EUR",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "sc.mtv.com",
+		"omnitureVisitorID" : "",
+		"omnitureDebugLogging" : "true",
+		"omnitureOfflineTrackingEnabled" : "true",
+		"omnitureOfflineHitLimit" : 1000,
+		"demdexEnabled" : "true",
+		"TVEEnabled" : "true",
+		"omnitureTimetrackingEnabled" : "true",
+		"appIsCoppaCompliant" : "false"
+    	}
+]
