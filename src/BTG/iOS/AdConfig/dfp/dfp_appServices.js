708b89b0f75b0bf90d7cf229d29f27267a89d972
antalav <veantala@gmail.com>
2014-10-16 19:16:48 -0400

    no message

---
+++ src/BTG/iOS/AdConfig/dfp/dfp_appServices.js
@@ -0,0 +1,55 @@
+[
+	{
+		"config" : {
+			"isCoppaCompliant" : false,
+			"audienceManagerEnabled": true,
+			"admsMeasureEnabled" : true,
+			"admsReportSuiteIDs"   : "viarnd",
+			"admsCharacterSet" : "ISO-8859-1",
+			"admsCurrencyCode" : "USD",
+			"admsSslEnabled" : true,
+			"admsLifecycleSessionTimeout" : 300,
+			"admsTrackingServer" : "sc.mtv.com",
+			"admsVisitorID" : "testVID",
+			"admsDebugLoggingEnabled" : true,
+			"admsOfflineTrackingEnabled" : true
+	
+		},
+		"adData" : {
+			"x" : 0,
+			"y"   : 0,
+			"width" : 300,
+			"height" : 50,
+			"appName" : "btg_test_app",
+			"sections" : "",
+			"isDFP" : true,
+			"enableCloseButton" : true,
+			"closeButtonInterval" : 5,
+			"isRefreshable" : false,
+			"refreshInterval" : 10,
+			"enableDefaultBumper" : true,
+			"enableCoppaAdBanner" : true,
+			"keyValues" : {
+				"!category" : "v1.2.3",
+				"ver"   : "v1.2.3",
+				"testmode" : "celtra_mobile_300x50"
+			},
+			"keychainKeyValues" : {
+				"aamSegments" : ""
+			},
+			"dfpData" : {
+				"metadata" : {
+					"property" : "mtv_news",
+					"hub" : "breaking",
+					"is_main" : true
+				},
+				"mappings" : {
+					"ad-unit" : {
+						"1" : "@property",
+						"2" : "@hub"
+					}
+				}		        	
+			}
+		}
+	}
+]
