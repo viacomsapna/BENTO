ebd0cd6e603af82cda620f0e73e98ce339a18b5f
antalav <veantala@gmail.com>
2014-10-31 08:03:28 -0400

    Update celtra_tablet_728x90.js

--- src/BTG/iOS/AdConfig/dart/celtra_tablet_728x90.js
+++ src/BTG/iOS/AdConfig/dart/celtra_tablet_728x90.js
@@ -20,9 +20,6 @@
 				"ver"   : "v1.2.3",
 				"testmode" : "celtra_tablet_728x90"
 			},
-			"keychainKeyValues" : {
-				"aamSegments" : ""
-			},
 			"dfpData" : {
 				"metadata" : {
 					"property" : "mtv_news",
