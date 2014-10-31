56a00f3ba520597ad617e85efb211a73057a1138
antalav <veantala@gmail.com>
2014-10-31 08:02:08 -0400

    Update adcade_cert_test_728x90.js

--- src/BTG/iOS/AdConfig/dart/adcade_cert_test_728x90.js
+++ src/BTG/iOS/AdConfig/dart/adcade_cert_test_728x90.js
@@ -20,9 +20,6 @@
 				"ver"   : "v1.2.3",
 				"testmode" : "adcade_cert_test_728x90"
 			},
-			"keychainKeyValues" : {
-				"aamSegments" : ""
-			},
 			"dfpData" : {
 				"metadata" : {
 					"property" : "mtv_news",
