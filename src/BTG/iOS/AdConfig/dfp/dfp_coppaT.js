f54be988034d4c8c818e3b1f9de5934a7dae5ee0
antalav <veantala@gmail.com>
2014-10-30 21:39:48 -0400

    no message

--- src/BTG/iOS/AdConfig/dfp/dfp_coppaT.js
+++ src/BTG/iOS/AdConfig/dfp/dfp_coppaT.js
@@ -8,31 +8,26 @@
 			"x" : 0,
 			"y"   : 0,
 			"width" : 300,
-			"height" : 250,
+			"height" : 50,
 			"appName" : "btg_test_app",
 			"sections" : "",
 			"isDFP" : true,
 			"enableCloseButton" : true,
 			"closeButtonInterval" : 5,
-			"isRefreshable" : false,
+			"isRefreshable" : true,
 			"refreshInterval" : 10,
-			"enableDefaultBumper" : true,
-			"enableCoppaAdBanner" : true,
-			"alternativeSizes" : [
-				{
-				"adWidth" : 300,
-				"adHeight" : 50
-				}
-			],
 			"keyValues" : {
 				"!category" : "v1.2.3",
 				"ver"   : "v1.2.3",
 				"testmode" : "celtra_mobile_300x50"
 			},
+			"keychainKeyValues" : {
+				"aamSegments" : ""
+			},
 			"dfpData" : {
 				"metadata" : {
-					"property" : "mtv",
-					"hub" : "shows",
+					"property" : "mtv_news",
+					"hub" : "breaking",
 					"is_main" : true
 				},
 				"mappings" : {
