928634c5dc05395baaed6877124374b78910f370
antalav <veantala@gmail.com>
2014-10-30 21:53:25 -0400

    no message

---
+++ src/BTG/iOS/AdConfig/dart/dart_AdbarFBumperF.js
@@ -0,0 +1,43 @@
+[
+	{
+		"config" : {
+			"isCoppaCompliant" : true
+		},
+		"adData" : {
+			"x" : 0,
+			"y"   : 0,
+			"width" : 300,
+			"height" : 50,
+			"appName" : "btg_test_app",
+			"sections" : "",
+			"isDFP" : false,
+			"enableCloseButton" : true,
+			"closeButtonInterval" : 5,
+			"isRefreshable" : false,
+			"refreshInterval" : 10,
+			"enableDefaultBumper" : false,
+			"enableCoppaAdBanner" :	false,
+			"keyValues" : {
+				"!category" : "v1.2.3",
+				"ver"   : "v1.2.3",
+				"testmode" : "celtra_mobile_300x50"
+			},
+			"keychainKeyValues" : {
+				"aamSegments" : ""
+			},
+			"dfpData" : {
+				"metadata" : {
+					"property" : "mtv_news",
+					"hub" : "breaking",
+					"is_main" : true
+				},
+				"mappings" : {
+					"ad-unit" : {
+						"1" : "@property",
+						"2" : "@hub"
+					}
+				}		        	
+			}
+		}
+	}
+]
