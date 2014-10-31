116bb059336b03da72a999c3e2c8e01ecfd8f9b9
antalav <veantala@gmail.com>
2014-10-30 21:31:38 -0400

    no message

---
+++ src/BTG/iOS/AdConfig/dart/celtra_audienceT.js
@@ -0,0 +1,39 @@
+[
+	{
+		"config" : {
+			"isCoppaCompliant" : false,
+			"audienceManagerEnabled": true
+		},
+		"adData" : {
+			"x" : 0,
+			"y"   : 0,
+			"width" : 300,
+			"height" : 250,
+			"appName" : "btg_test_app",
+			"sections" : "/test/mobile300-250",
+			"isDFP" : false,
+			"enableCloseButton" : false,
+			"closeButtonInterval" : 5,
+			"isRefreshable" : false,
+			"refreshInterval" : 10,
+			"slot" : 1,
+			"keyValues" : {
+				"testkey"   : "key2.1",
+				"testmode" : "celtra_mobile_300x250"
+			},
+			"dfpData" : {
+				"metadata" : {
+					"property" : "mtv_news",
+					"hub" : "breaking",
+					"is_main" : true
+				},
+				"mappings" : {
+					"ad-unit" : {
+						"1" : "@property",
+						"2" : "@hub"
+					}
+				}		        	
+			}
+		}
+	}
+]
