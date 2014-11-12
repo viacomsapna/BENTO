ce54417ae1c03f027ba90f5012bb11f924398746
antalav <veantala@gmail.com>
2014-11-12 17:22:01 -0500

    no message

---
+++ src/BTG/iOS/Adms/appConfig_OmnitureSuite.js
@@ -0,0 +1,22 @@
+[
+	{
+		"admsMeasureEnabled" : true,
+		"admsReportSuiteIDs"   : "viarnd-Test",
+		"admsCharacterSet" : "ISO-8859-1",
+		"admsCurrencyCode" : "USD-TEST",
+		"admsSslEnabled" : true,
+		"admsLifecycleSessionTimeout" : 300,
+		"admsTrackingServer" : "Test.mtv.com",
+		"admsVisitorID" : "testVID",
+		"admsDebugLoggingEnabled" : false,
+		"admsOfflineTrackingEnabled" : false,
+		"audienceManagerEnabled" : true,
+		"comScoreEnabled" : true,
+		"channel" : "remote sandbox channel",
+		"admsContextData"   : {
+			"contextDataKey1onconfig"    : "testcontextvalue1onconfig",
+			"contextDataKey2onconfig"    : "testcontextvalue2onconfig",
+			"contextDataKey1trackpage"    : "testcontextvalue1onconfig"
+		}
+	}
+]
